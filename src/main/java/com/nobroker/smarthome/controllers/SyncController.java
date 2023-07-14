471d9deae1b8b38514fc4ee5b2508758c336ba14
Divyanshu Raj <divyanshuraj5050@gmail.com>
2023-07-14 23:12:34 +0530

    add fan state change api

--- src/main/java/com/nobroker/smarthome/controllers/SyncController.java
+++ src/main/java/com/nobroker/smarthome/controllers/SyncController.java
@@ -3,11 +3,14 @@ package com.nobroker.smarthome.controllers;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.nobroker.smarthome.firebase.Firebase;
+import org.hibernate.internal.build.AllowPrintStacktrace;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/sync")
@@ -15,12 +18,17 @@ public class SyncController {
     @Autowired
     private Firebase firebase;
 
-    @GetMapping("/bulb/{state}")
-    public String syncState(@PathVariable String state) throws Exception {
+    @GetMapping("/{appliance}/{state}")
+    public String syncState(@PathVariable String state, @PathVariable String appliance) throws Exception {
         Firestore firestore  = firebase.getFireStore();
 
+
+        if(!appliance.equalsIgnoreCase("bulb") &&  !appliance.equalsIgnoreCase("fan")){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid request");
+        }
+
         // Update an existing document
-        DocumentReference docRef = firestore.collection("smarthomes").document("bulb");
+        DocumentReference docRef = firestore.collection("smarthomes").document(appliance);
 
 
         ApiFuture<WriteResult> future = docRef.update("state", state);
